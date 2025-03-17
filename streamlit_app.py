d610b8b96163f42d455b50963ddb5a25b56a5415
Lavanya786-lab <m_pavankalyan@yahoo.com>
2025-03-16 23:23:02 -0400

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,6 +19,7 @@ my_dataframe = session.table(
      "smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON'))
 pd_df = my_dataframe.to_pandas()
  
+ 
 ingredients_list = st.multiselect(
      "Choose up to 5 ingredients",
      my_dataframe,
@@ -35,9 +36,9 @@ if ingredients_list:
          search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
          st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
  
+         #fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_chosen)
          fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+search_on)
-    
-     fv_dt = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
+         fv_dt = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
  
      st.write(ingredients_string)
  
@@ -51,4 +52,3 @@ if ingredients_list:
  
          session.sql(my_insert_stmt).collect()
          st.success('Your Smoothie is ordered, '+name_on_order+'!', icon="✅")
- 
